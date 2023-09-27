fff2fb92ec50c19b4fb7a8dc8bc224a445cd49b2
hp <hp@DESKTOP-20GUFEH>
2023-09-27 08:43:47 +0700

    commit pertama -project praktikum alpro

---
+++ PrakAlpro2311531009/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author hp
+ *
+ */
+module PrakAlpro2311531009 {
+}
---
+++ PrakAlpro2311531009/src/pekan1/HelloWorld.java
@@ -0,0 +1,10 @@
+package pekan1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// cencetak string hello world
+		System.out.println("Hello World");
+	}
+
+}
