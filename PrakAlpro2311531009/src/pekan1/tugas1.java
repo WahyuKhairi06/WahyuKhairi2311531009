77a719e71c6bc959048439ce62b6c18114916131
Lenovo <Lenovo@Komputer11>
2023-10-03 16:20:52 +0700

    tugas 1

---
+++ PrakAlpro2311531009/src/pekan1/tugas1.java
@@ -0,0 +1,11 @@
+package pekan1;
+
+public class tugas1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("2311531009_Wahyu Khairi");
+
+	}
+
+}
