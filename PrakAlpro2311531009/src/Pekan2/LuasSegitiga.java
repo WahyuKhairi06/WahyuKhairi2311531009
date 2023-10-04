f77506c7f4542741e835d23ce726bb4d1088bd94
Lenovo <Lenovo@Komputer11.mshome.net>
2023-10-04 08:46:27 +0700

    commit

---
+++ PrakAlpro2311531009/src/Pekan2/LuasSegitiga.java
@@ -0,0 +1,24 @@
+package Pekan2;
+
+import java.util.Scanner;
+
+public class LuasSegitiga {
+
+	public static void main(String[] args) {
+		int alas,tinggi;
+		double luas;
+		Scanner input=new Scanner(System.in);
+		System.out.println("Inputkan Alas=");
+		alas=input.nextInt();
+		System.out.println("inputkan tinggi=");
+		tinggi=input.nextInt();
+		input.close();
+		luas=0.5*alas*tinggi;
+		System.out.println("Luas Segitiga Adalah:"+luas);
+		
+		
+				
+
+	}
+
+} 
