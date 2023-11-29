19772d929bac5bad9c38f5e771f7336d54ab1b81
Lenovo <Lenovo@Y.mshome.net>
2023-11-29 09:29:13 +0700

    Commit

--- PrakAlpro2311531009/src/pekan6/TugasPerulanganPekan6.java
+++ PrakAlpro2311531009/src/pekan6/TugasPerulanganPekan6.java
@@ -15,12 +15,12 @@ public class TugasPerulanganPekan6 {
 		        for ( i = 1; i <= n; i++) {
 		        	if (i % 2 != 0 ) {
 		        		for (j = 1; j <= uji + 1 ; j++) {
-		        			System.out.print("_");
+		        			System.out.print("_ ");
 		        		}
 		       }else {
 		    	       System.out.print(" ");
 		    	       for (k = 1; k <= uji; k++ ) {
-		    	       System.out.print("_");
+		    	       System.out.print("_ ");
 		 }
 	   }
 	 System.out.println();
